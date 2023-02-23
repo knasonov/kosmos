08ad41985251551778d44d73a819759b7bd0a219
Konstantin Nasonov <knasonov@123prod.local>
2023-02-23 14:04:08 +0300

    fixed

--- server_listen.py
+++ server_listen.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template,send_from_directory
+from flask import Flask, request, render_template,send_from_directory, send_file
 from flask_cors import cross_origin
 import json
 import os
